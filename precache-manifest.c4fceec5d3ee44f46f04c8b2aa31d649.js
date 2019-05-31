bc7b9c804bac892501e202ec66e596a4b33c2e86
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2019-05-31 16:35:25 +0100

    initial commit

---
+++ precache-manifest.c4fceec5d3ee44f46f04c8b2aa31d649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b25c1328f6d1f4ce41fd78c917a0fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c22101a9b384add56994",
+    "url": "/static/css/main.2da23be0.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c22101a9b384add56994",
+    "url": "/static/js/main.2f0ced96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c905c11d28e3c23ff003fa0ce24d806",
+    "url": "/static/media/logo.7c905c11.svg"
+  }
+]);
